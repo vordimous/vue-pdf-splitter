064a3bf12b3540723adea27106a19894cf4bb0e4
AJ <vordimous@outlook.com>
2025-01-18 11:28:30 -0500

    feat: add pdf splitting component

---
+++ README.md
@@ -0,0 +1,36 @@
+# pdf-test
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vite.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+
+git init
+git commit -m "feat: add pdf splitting component"
+git branch -M main
+git remote add origin git@github.com:vordimous/vue-pdf-splitter.git
+git push -u origin main
---
+++ src/components/PdfSplitter.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="pdf-document" v-for="(pages, index) in newPdfs" :key="index"
+        :ref="(el) => (initUseDropZoneSortable(index, el))">
+        <div v-for="page in pages" :key="page" class="pdf-document-page draggable">
+            <VuePDF :pdf="pdf" :page="page" :width="100" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { nextTick, watch, reactive, Reactive } from 'vue'
+import { VuePDF, usePDF } from '@tato30/vue-pdf'
+import '@tato30/vue-pdf/style.css'
+
+const { pdf, pages: totalPdfPages } = usePDF('https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf')
+import { useDropZone } from '@vueuse/core'
+import { useSortable, moveArrayElement } from '@vueuse/integrations/useSortable'
+
+const newPdfs: Reactive<Reactive<number[]>[]> = reactive<Reactive<number[]>[]>([[]])
+var dragOrigIndex = 0
+var dragDestIndex = 0
+
+// when the PDF has updated the pages, load them into the splitter
+watch(totalPdfPages, (p) => {
+    newPdfs[0].push(...Array.from({ length: p - 1 }, (_, index) => index + 1))
+})
+
+// add a new empty document if one does not exist
+watch(newPdfs, (p) => {
+    var emptyDocs = p.filter((a) => a.length === 0).length
+    if (emptyDocs === 0) {
+        nextTick(() => {
+            newPdfs.push([])
+        })
+
+    }
+})
+
+function initUseDropZoneSortable(index: number, el: HTMLElement | any | null) {
+    var newPdfPages = newPdfs[index]
+    useDropZone(el, {
+        onDrop: () => {
+            dragDestIndex = index
+        },
+    })
+    useSortable(el, newPdfPages, {
+        onStart: () => {
+            dragOrigIndex = index
+        },
+        onUpdate: (e) => {
+            // only update the sortable order if the drop is in the same document
+            if (dragOrigIndex === dragDestIndex && e.oldIndex !== undefined && e.newIndex !== undefined) {
+                moveArrayElement(newPdfs[dragOrigIndex], e.oldIndex, e.newIndex)
+            }
+            nextTick(() => {
+            })
+        },
+        onEnd: (e) => {
+            if (dragOrigIndex !== dragDestIndex && e.oldIndex !== undefined) {
+                var dragOrig = newPdfs[dragOrigIndex]
+                var dragDest = newPdfs[dragDestIndex]
+
+                // move the page from its starting index in the Origin to the end of the Dest
+                dragDest.push(dragOrig[e.oldIndex])
+                dragOrig.splice(e.oldIndex, 1);
+
+            }
+        },
+    })
+
+}
+
+</script>
+
+<style>
+.draggable {
+    cursor: grab;
+}
+
+.pdf-document {
+    background-color: grey;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: left;
+    align-items: center;
+    width: 80vw;
+    min-height: 75px;
+    margin: 5px;
+}
+
+.pdf-document .pdf-document-page {
+    background-color: black;
+    padding: 3px;
+    margin: 5px;
+}
+</style>

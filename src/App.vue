331bef049c059d5f2203beb9c06d68736b1bbe5f
AJ <vordimous@outlook.com>
2025-02-06 16:33:16 -0500

    feat: use one pdf to manage all of the pages

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
 import PdfSplitter from './components/PdfSplitter.vue'
+const urls = [
+  'https://pdf-lib.js.org/assets/american_flag.pdf',
+  'https://pdf-lib.js.org/assets/us_constitution.pdf',
+  'https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf', 
+  // 'https://pdf-lib.js.org/assets/with_large_page_count.pdf',
+]
 </script>
 
 <template>
 
   <main>
-    <PdfSplitter />
+    <PdfSplitter :preLoadPdfUrls="urls" />
   </main>
 </template>
 
--- src/components/PdfSplitter.vue
+++ src/components/PdfSplitter.vue
@@ -1,137 +1,233 @@
 <template>
-    <input type="range" min=".15" max="1" step=".05" v-model="scale" class="slider">
-    <div class="pdf-document" v-for="(doc, index) in docs" :id="`new-pdf-document-${index}`" :key="index"
-        :ref="(el) => { initUseDropZone(doc, el); initUseSortable(doc, el) }":class="{ 'pdf-document-drop': doc.hover }">      
-      <div v-if="docs.length != index + 1 " class="pdf-document-header">
-        <button @click="() => removeDoc(doc, index)">Delete</button>
-        <input type="text" :value="doc.name"/>
-      </div>
-      <div v-if="doc.pages.length === 0" class="pdf-document-new">
-        <p>Drag Here to Create</p>
-      </div>
-
-      <div v-for="(page, index) in doc.pages" :key="page?.pageNumber" class="pdf-document-page">
-        <VuePDF :pdf="pdf" :page="page.pageNumber" :scale="scale" :rotation="page.rotation" />
-        <button @click="() => page.rotation -= 90"><</button><button @click="() => page.rotation += 90">></button><button @click="removePage(doc, index)">x</button>
-      </div>
+  <button @click="() => addPageGroup({})">New Page</button>
+  <input type="range" min=".15" max="1" step=".05" v-model="scale" class="slider">
+  <div class="pdf-document" v-for="(pageGroup, index) in allPageGroups" :id="`new-pdf-document-${index}`" :key="index"
+    :ref="(el) => pageGroup.pageElRef = el" :class="{ 'pdf-document-drop': pageGroup.hover }">
+    <div class="pdf-document-header">
+      <button @click="() => removeDoc(pageGroup, index)">Delete</button>
+      <input type="text" :value="pageGroup.name" />
+      <button @click="() => downloadDoc(pageGroup, index)">Download</button>
     </div>
+    <div v-if="pageGroup.pages.length === 0" class="pdf-document-new">
+      <p>Drag Here to Create</p>
+    </div>
+
+    <div v-for="(page, index) in pageGroup.pages" :key="page?.pageNumber" class="pdf-document-page">
+      <VuePDF :pdf="pdfPageViewer" :page="page.pageNumber" :scale="scale" :rotation="page.rotation" />
+      <button @click="() => page.rotation -= 90"><</button><button @click="() => page.rotation += 90">></button><button
+            @click="removePage(pageGroup, index)">x</button>
+          <p>{{ page.pageNumber }}</p>
+    </div>
+  </div>
+  <iframe :src="iframeSrc" style="width: 100%; height: 100%;"></iframe>
 </template>
 
 <script lang="ts" setup>
-import { nextTick, watch, reactive, Reactive, ref, Ref } from 'vue'
-import { VuePDF, usePDF } from '@tato30/vue-pdf'
-
-const { pdf, pages: totalPdfPages } = usePDF('https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf')
-import { useDropZone } from '@vueuse/core'
+import * as PDFJS from 'pdfjs-dist';
+import LegacyWorker from 'pdfjs-dist/legacy/build/pdf.worker.min?url';
+PDFJS.GlobalWorkerOptions.workerSrc = LegacyWorker
+import { PDFDocument } from 'pdf-lib'
+import { nextTick, watchEffect, reactive, Reactive, ref, Ref, onMounted, watch } from 'vue'
+import { VuePDF } from '@tato30/vue-pdf'
+import { useDropZone, watchOnce } from '@vueuse/core'
 import { useSortable, moveArrayElement } from '@vueuse/integrations/useSortable'
+
+const props = defineProps({
+  preLoadPdfUrls: Array<string>,
+})
+
+const pdfPageViewer:Ref<PDFJS.PDFDocumentLoadingTask> = ref()
+var pdfDataDocument:PDFDocument
+
+async function addPdfPagesFromUrl(url: string): Promise<number> {
+  const arrayBuffer = await fetch(url).then(res => res.arrayBuffer())
+  return await addPdfPages(arrayBuffer)
+}
+
+async function addPdfPages(arrayBuffer:ArrayBuffer): Promise<number> {
+  var pdfDocument = await PDFDocument.load(arrayBuffer)
+  var copiedPages = await pdfDataDocument.copyPages(pdfDocument, numArray(pdfDocument.getPageCount()))
+  copiedPages.forEach((page) => {
+    pdfDataDocument.addPage(page)
+  })
+  return copiedPages.length
+}
+
+async function renderPdfPages() {
+  const loadingTask = PDFJS.getDocument(await pdfDataDocument.save())
+  pdfPageViewer.value = loadingTask
+}
+
+function numArray(size: number, shift: number = 0): number[] {
+  return Array.from({ length: size }, (_, index) => index + shift)
+}
+
 
 type Page = {
   pageNumber: number;
   rotation: number;
 };
-type Doc = {
+type PageGroup = {
   index: number;
   pages: Reactive<Page[]>;
   name: string;
   hover: boolean;
+  pageElRef: HTMLElement;
 };
-function addEmptyDoc() {
-  docs.push({ index: docs.length, pages: [], name: "", hover: false })
-}
-const docs: Reactive<Doc[]> = reactive<Reactive<Doc[]>>([])
-addEmptyDoc()
 
-// when the PDF has updated the pages, load them into the splitter
-watch(totalPdfPages, (p) => {
-  Array.from({ length: p - 1 }, (_, index) => index + 1).forEach((n) => {
-      docs[0].pages.push({pageNumber: n, rotation: 0})
+function addPageGroup(pg: PageGroup | any) {
+  allPageGroups.value.push({
+    pages: [],
+    name: "",
+    ...pg,
+    hover: false,
   })
-  docs[0].name = "tracemonkey-pldi-09.pdf"
+}
+
+const allPageGroups: Ref<PageGroup[]> = ref<PageGroup[]>([])
+
+onMounted(async () => {
+  pdfDataDocument = await PDFDocument.create()
+  var loadedDocs = await Promise.all(props.preLoadPdfUrls.map(async (url) => {
+    var numOfPages = await addPdfPagesFromUrl(url)
+    return {
+      url,
+      numOfPages
+    }
+  }))
+
+  await renderPdfPages()
+  var allPageNums = numArray(pdfDataDocument.getPageCount(), 1)
+  loadedDocs.forEach(({
+      url,
+      numOfPages
+    }) => {
+
+    addPageGroup({
+      pages: numArray(numOfPages, 1).map(() => ({ pageNumber: allPageNums.shift(), rotation: 0 })),
+      name: url,
+    })
+  })
+  addPageGroup({})
   nextTick(() => {
-    addEmptyDoc()
+    allPageGroups.value.forEach(initDragDrop);
   })
 })
 
+var dragOrig: PageGroup | undefined
+var dragDest: PageGroup | undefined
 
-var dragOrig: Doc | undefined
-var dragDest: Doc | undefined
-
-function initUseDropZone(doc: Doc, el: HTMLElement | any | null) {
-  useDropZone(el, {
+function initDragDrop(pg: PageGroup) {
+  if (pg.pageElRef) {
+    console.log('Init drag drop', pg.name)
+    initUseDropZone(pg)
+    initUseSortable(pg)
+  }
+}
+function initUseDropZone(pageGroup: PageGroup) {
+  useDropZone(pageGroup.pageElRef, {
     onDrop: () => {
-      dragDest = doc
-      doc.hover = false
+      dragDest = pageGroup
+      pageGroup.hover = false
+      console.log("onDrop.hover", pageGroup.name)
     },
     onOver: () => {
       nextTick(() => {
-        doc.hover = true
+        pageGroup.hover = true
+        console.log("onOver.hover", pageGroup.name)
       })
     },
     onLeave: () => {
       nextTick(() => {
-        doc.hover = false
+        pageGroup.hover = false
+        console.log("onLeave.hover", pageGroup.name)
       })
     }
   })
 }
-function initUseSortable(doc: Doc, el: HTMLElement | any | null) {
-  useSortable(el, doc.pages, {
+function initUseSortable(pageGroup: PageGroup) {
+  console.log("initUseSortable", pageGroup.pages.map(({ pageNumber }) => (pageNumber)))
+  useSortable(pageGroup.pageElRef, pageGroup.pages, {
     onStart: (e) => {
-      dragOrig = doc
-      dragDest = doc
+      dragOrig = pageGroup
+      dragDest = pageGroup
     },
     onUpdate: (e) => {
       // only update the sortable order if the drop is in the same document
+      console.log("onUpdate")
       if (dragOrig !== undefined && dragDest !== undefined && dragOrig === dragDest
         && e.oldIndex !== undefined && e.newIndex !== undefined) {
-        moveArrayElement(doc.pages, e.oldIndex, e.newIndex)
+        moveArrayElement(pageGroup.pages, e.oldIndex, e.newIndex)
+        console.log("onUpdate", pageGroup.pages.map(({ pageNumber }) => (pageNumber)))
       }
     },
     onEnd: (e) => {
       if (dragOrig !== undefined && dragDest !== undefined
         && dragOrig !== dragDest && e.oldIndex !== undefined) {
+          console.log("onEnd")
         // move the page from its starting index in the Origin to the end of the Dest
         const pageArrayIndex = e.oldIndex - 1
         if (dragOrig.pages[pageArrayIndex]?.pageNumber > 0) {
-            dragDest.pages.push(dragOrig.pages[pageArrayIndex])
-            dragOrig.pages.splice(pageArrayIndex, 1);
+          dragDest.pages.push(dragOrig.pages[pageArrayIndex])
+          dragOrig.pages.splice(pageArrayIndex, 1)
+          console.log("onEnd", pageGroup.pages.map(({ pageNumber }) => (pageNumber)))
         }
 
       }
-      // Add new doc
-      if (docs.filter((a) => a.pages.length === 0).length === 0) {
-        addEmptyDoc()
-      }
+      // Add new pageGroup
+      // if (allPageGroups.filter((a) => a.pages.length === 0).length === 0) {
+      //   addEmptyPageGroup()
+      // }
     },
   })
 }
 
-function removePage(doc: Doc, index: Number) {
-    doc.pages.splice(index, 1)
+function removePage(pageGroup: PageGroup, index: Number) {
+  pageGroup.pages.splice(index, 1)
 }
+function removeDoc(pageGroup: PageGroup, index: Number) {
+  pageGroup.splice(index, 1)
+}
+async function downloadDoc(pageGroup: PageGroup) {
 
-function removeDoc(doc: Doc, index: Number) {
-    docs.splice(index, 1)
+  const newPdf = await PDFDocument.create()
+
+  // console.log(newPdf.getPageCount())
+  // for (const page of pageGroup.pages) {
+  //   var pageIndex = page.pageNumber - 1
+  //   console.log("num", page.pageNumber, pageIndex)
+  //   var [copiedPage] = await newPdf.copyPages(getPdfDocument(page._pdfKey), [pageIndex])
+  //   newPdf.addPage(copiedPage)
+  // }
+  // console.log(newPdf.getPageCount())
+
+  console.log("loading")
+  iframeSrc.value = await newPdf.saveAsBase64({ dataUri: true });
+  console.log("loaded")
+
 }
 
 const scale = ref(.20)
+const iframeSrc = ref('')
 
 </script>
 
 <style>
 body {
-    background-color: whitesmoke;
+  background-color: whitesmoke;
 }
+
 .pdf-document {
-    background-color: lightgray;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: left;
-    align-items: center;
-    width: 80vw;
-    min-height: 75px;
-    margin: 5px;
+  background-color: lightgray;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: left;
+  align-items: center;
+  width: 80vw;
+  min-height: 75px;
+  margin: 5px;
 }
+
 .pdf-document .pdf-document-header {
   border: 1px solid grey;
   padding: 3px;
@@ -157,6 +253,7 @@ body {
   padding: 3px;
   margin: 3px;
 }
+
 .pdf-document-drop {
   background-color: var(--gray-200);
 }
